0013d5b37c3e306bfb312346a510a020c0e88f67
mudssky <mudssky@gmail.com>
2018-12-24 05:16:20 +0800

    fix a word mistake
    Former-commit-id: 51d23a881f3386cfb59e889784b86288aaa4881d

--- cleanTorrent/cleanTorrent.ps1
+++ cleanTorrent/cleanTorrent.ps1
@@ -1,1 +1,1 @@
-﻿$num=0;ls -Recurse -Force |foreach{$num++; if ($_.Name.EndsWith('.torrent')){"clean : $($_.FullName)";del  -Force --LiteralPath $_.FullName}} ; "fileCounts:$num";pause
+﻿$num=0;ls -Recurse -Force |foreach{$num++; if ($_.Name.EndsWith('.torrent')){"clean : $($_.FullName)";del  -Force -LiteralPath $_.FullName}} ; "fileCounts:$num";pause
