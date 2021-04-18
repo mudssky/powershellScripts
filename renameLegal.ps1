d17a37613c1a485b3d2835634f6b32f03ed801cb
mudssky <mudssky@gmail.com>
2021-04-19 01:42:46 +0800

    add get-reversedmap  renameLegal

---
+++ renameLegal.ps1
@@ -0,0 +1,61 @@
+
+param(
+[switch]$reverse
+)
+
+<#
+.SYNOPSIS
+    获取一个和输入哈希表key和value调换位置的哈希表
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    PS C:\> Get-ReversedMap -inputMap $xxxMap
+    Explanation of what the example does
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    General notes
+#>
+function Get-ReversedMap() {
+    param (
+        $inputMap
+    )
+    $reversedMap = @{}
+    foreach ($key in $inputMap.Keys) {
+        $reversedMap[$inputMap[$key]]=$key
+    }
+    return $reversedMap
+}
+# 对windows的非法字符进行转换，利用全角字符替换半角字符
+$replaceTableMap  = @{
+    '\'='、';
+    '/'='／';
+    ':'='：';
+    '?'='？';
+    '<'='《';
+    '>'='》';
+    '|'='｜';
+    '*'='＊';
+    '"'='“';
+}
+
+# 如果反向转换的话，就会调用把map反向的函数
+if ($reverse){
+    $replaceTableMap = Get-ReversedMap -inputMap $replaceTableMap
+}
+
+# Get-ReversedMap -inputMap $replaceTableMap
+Get-ChildItem -File | ForEach-Object {
+    $newName=''
+    for ($i = 0; $i -lt $_.Name.Length; $i++) {
+        $curChar =  $_.Name[$i]
+        if ($curChar -in $replaceTableMap.Keys){
+            $newName+=$replaceTableMap[$curChar]
+        }else{
+            $newName+=$curChar
+        }
+    };
+    Rename-Item  -LiteralPath $_.Name   -NewName $newName
+}
