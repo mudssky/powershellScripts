5aedd4287fa74eabe3e78c29240588aeafc1b404
mudssky <mudssky@gmail.com>
2021-07-06 20:27:17 +0800

    update ffmpegPreset
    Former-commit-id: 69cf9b73ccfe4b7cbe25f7a81e761118fcfd2581

--- ffmpegPreset.ps1
+++ ffmpegPreset.ps1
@@ -1,41 +1,42 @@
 <#
 .synopsis
-¹ØÓÚffmpegÑ¹ÖÆÊÓÆµµÄ¿ìËÙÔ¤Éè£¬Ö÷ÒªÓÃÓÚËõÐ¡Ö±²¥Â¼ÖÆÊÓÆµµÄÌå»ý¡£
+å…³äºŽffmpegåŽ‹åˆ¶è§†é¢‘çš„å¿«é€Ÿé¢„è®¾ï¼Œä¸»è¦ç”¨äºŽç¼©å°ç›´æ’­å½•åˆ¶è§†é¢‘çš„ä½“ç§¯ã€‚
 .example
 ffmpegPreset.ps1 -path 'input.flv'
 
-³£¼ûÊ¹ÓÃ·½Ê½
+å¸¸è§ä½¿ç”¨æ–¹å¼
 ls *.flv|%{ffmpegPreset.ps1 -path $_.Name}
 ls *.flv|%{ffmpegPreset.ps1 -preset '720p28' -path $_.Name}
 ls *.flv|%{ffmpegPreset.ps1 -preset '720p' -path $_.Name}
 #>
 param(
     [string]$path,
-    [string]$ffmpegStr='-vcodec libx264 -acodec copy -crf 23  -preset veryfast',
-	[string]$outpath,
-    [string]$preset=''
+    [string]$ffmpegStr = '-vcodec libx264 -acodec copy -crf 23  -preset veryfast',
+    [string]$outpath,
+    [string]$preset = ''
 	
 )
 
 #[string]$ffmpegStr='-vcodec libx264 -acodec copy -crf 23 -r 30 -preset veryfast',
-$presetTable=@{'veryslow'='-vcodec libx264 -acodec copy -crf 23 -r 30 -preset veryslow';
-    ''='-vcodec libx264 -acodec copy -crf 23  -preset veryfast';
-    'x265'='-vcodec libx265 -acodec copy -crf 23  -preset fast';
-    'hevc'='-vcodec libx265 -acodec copy -crf 28  -preset fast';
-    '720p'='-vcodec libx264 -acodec copy -crf 23  -preset veryfast -r 30 -s 1280*720';
-    '720p28'='-vcodec libx264 -acodec copy -crf 28  -preset veryfast -r 30 -s 1280*720';
-    '480p'='-vcodec libx264 -acodec copy -crf 23  -preset veryfast -r 30 -s 854*480';
-    'copy'='-c copy'
+$presetTable = @{'veryslow' = '-vcodec libx264 -acodec copy -crf 23 -r 30 -preset veryslow';
+    ''                      = '-vcodec libx264 -acodec copy -crf 23  -preset veryfast';
+    'm2ts'                  = ' -vcodec libx265 -acodec flac ';
+    'x265'                  = '-vcodec libx265 -acodec copy -crf 23  -preset fast';
+    'hevc'                  = '-vcodec libx265 -acodec copy -crf 28  -preset fast';
+    '720p'                  = '-vcodec libx264 -acodec copy -crf 23  -preset veryfast -r 30 -s 1280*720';
+    '720p28'                = '-vcodec libx264 -acodec copy -crf 28  -preset veryfast -r 30 -s 1280*720';
+    '480p'                  = '-vcodec libx264 -acodec copy -crf 23  -preset veryfast -r 30 -s 854*480';
+    'copy'                  = '-c copy'
 }
 
-$ffmpegStr=$presetTable[$preset]
+$ffmpegStr = $presetTable[$preset]
 
 #$extLenth=$path.Length-$path.LastIndexOf('.')
-if ( -not $outpath){
-$outpath=$path.Substring(0,$path.LastIndexOf('.'))+'.mp4'
+if (-not $outpath) {
+    $outpath = $path.Substring(0, $path.LastIndexOf('.')) + '.mp4'
 }
 
-$commandStr = ("ffmpeg.exe -i  '{0}'  {1}  '{2}' "-f $path,$ffmpegStr,$outpath)
+$commandStr = ("ffmpeg.exe -i  '{0}'  {1}  '{2}' " -f $path, $ffmpegStr, $outpath)
 
 
 Invoke-Expression $commandStr
