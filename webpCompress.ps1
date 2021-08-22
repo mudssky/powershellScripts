ed901f0aa7a6b7ac89bc1843c2c0a73bc5237604
mudssky <mudssky@gmail.com>
2021-08-22 21:30:17 +0800

    webpCompress utf8
    Former-commit-id: c1bb82b555324fd7354f42c14abfbea56dc94b3c

--- webpCompress.ps1
+++ webpCompress.ps1
@@ -1,30 +1,29 @@
 <#
 .synopsis
-����ת��ռ�ÿռ�����ͼƬ
+批量转换占用空间过大的图片
 .example
 
-ʹ�÷�������
+使用方法如下
 allPngtoWebp.ps1 -limitSize 10mb -paramStr '-codec libwebp -vf scale=iw/4:ih/4 -lossless 1 -quality 100 -pix_fmt yuv420p'
-
 
 #>
 param(
-# ��Ҫѹ����ͼƬ��ֵ��С��С�������ֵ��ͼƬ������ѹ��
+# 需要压缩的图片阈值大小，小于这个数值的图片不进行压缩
 [string]$targetPath='.',
 [int]$limitSize=3mb,
 [string]$paramStr=' ',
 # [string]$paramStr='-codec libwebp -pix_fmt yuv420p -vf scale=iw:ih  -lossless 0 -quality 75',
-# ��ʵ���ö����ã�Ĭ�ϵ����þ�����õģ�Ĭ��ѹ�Ƴ����ľ���yuv420p
+# 其实不用多设置，默认的设置就是最好的，默认压制出来的就是yuv420p
 #[string]$paramStr='-codec libwebp  -vf scale=iw:ih  -lossless 0 -quality 75',
 [switch]$noDelete,
 [switch]$lossless
 )
 
-#ֻѹ��png��ʽ��ͼƬ���ȼ���Ŀ¼�µ�pngͼƬ
+#只压缩png格式的图片，先检索目录下的png图片
 $count=0
 $supportExt='.png','.bmp','.tif','.jpg'
 if($lossless){
-# -q ������ѹ����ʱ���Ӱ��ѹ����ʱ�䣬Ĭ��Ҳ��75����Ϊ100��ã�����Ϊ�˼��ٵ������㸺�������ﲻ���Ķ�
+# -q 在无损压缩的时候会影响压缩的时间，默认也是75，设为100最好，但是为了减少电脑运算负担，这里不做改动
 $paramStr+=' -lossless 1 '
 $limitSize=0
 }
