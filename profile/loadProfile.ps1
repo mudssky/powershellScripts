39d75a791d0f262eb87926751cd2f5fa66fff7d3
mudssky <mudssky@gmail.com>
2022-07-25 00:22:57 +0800

    ✨ feat: update load profile
    Former-commit-id: df5d766f5ac36c68cca7aa5a6ec7ea23e4335347

--- profile/loadProfile.ps1
+++ profile/loadProfile.ps1
@@ -2,5 +2,9 @@
 $currentScriptPath =  $MyInvocation.MyCommand.Definition
 $currentScriptFolder = Split-Path  -Parent   $currentScriptPath 
 
+if (-not (Test-Path  -Path $profile )){
+	Write-Host ('创建profile文件: {0}' -f $profile) -ForegroundColor Green
+	New-Item -Path $profile -Force
+}
 
 Set-Content -Path $profile  -Value (Get-Content $currentScriptFolder/profile.ps1)
