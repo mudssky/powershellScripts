ff7c9a05969904a1f0ebbb2ccda7ab6cccf2e3ef
mudssky <mudssky@gmail.com>
2023-03-15 21:21:21 +0800

    ✨ feat: add jupytext
    Former-commit-id: 2597f40ce25e8263b62d99f9ee9448bc507af94e

--- jupyconvert.ps1
+++ jupyconvert.ps1
@@ -3,5 +3,8 @@ param(
 	[string]$Path = '.'
 )
 
+trap {
+	"error found"
+}
 
-Get-ChildItem -Recurse -Path $Path  -Filter *.ipynb | ForEach-Object { jupytext --set-formats ipynb, py:percent $_.FullName }
+Get-ChildItem -Recurse -Path $Path  -Filter *.ipynb | ForEach-Object { jupytext --set-formats 'ipynb,py:percent' $_.FullName }
