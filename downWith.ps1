7e26b98ae900dd8a04dfe0208facaf1953fb110a
mudssky <mudssky@gmail.com>
2023-06-30 15:00:38 +0800

    ✨ feat: update scripts
    Former-commit-id: 564fd5d64f563e1e9f6f657cb3386d9c3ae93e58

--- downWith.ps1
+++ downWith.ps1
@@ -1,36 +1,50 @@
 <#
-.example
+ .SYNOPSIS
+ 使用yt-dlp下载视频
+.DESCRIPTION
+使用yt-dlp下载视频
+.EXAMPLE
+指定下载工具
 downWithYoutubedl.ps1 -startNum 0 -paramStr '-i' -tool you-get
+.EXAMPLE 
+下载前3个
+downWith.ps1 -endNum 3 -paramStr '--playlist' -tool you-get
+.EXAMPLE
 
-��ʹ��you-get��ʱ�򣬵��͵�ʹ�÷����ǣ�downWith.ps1 -endNum 3 -paramStr '--playlist' -tool you-get
-
+带有cookie下载
+downWith.ps1  -paramStr  '--cookies-from-browser chrome'
+或者
+downWith.ps1  -withCookie
 #>
+[CmdletBinding()]
 param(
-[int]$startNum = 0,
-[int]$endNum=-1,
-[string]$paramStr = '',
-[string]$tool = 'yt-dlp',
-[string]$url=''
+    [int]$startNum = 0,
+    [int]$endNum = -1,
+    [string]$paramStr = '',
+    [string]$tool = 'yt-dlp',
+    [string]$url = '',
+    [switch]$withCookie
 )
-$str =''
-if($url){
-$str =$url
-}else{
-$str = Get-Clipboard;
+$str = ''
+if ($url) {
+    $str = $url
+}
+else {
+    $str = Get-Clipboard;
 }
 
 $linkList = $str -split '\n'
-# endNum��Ĭ��ֵ��-1��Ҳ���ǲ����ã���ʱendNumΪĬ��ֵ�����б�����
-if ($endNum -eq -1){
+if ($endNum -eq -1) {
     $endNum = $linkList.Length
 }
-for($i=$startNum;$i -lt $endNum;$i+=1){
+
+if ($withCookie) {
+    $paramStr += ' --cookies-from-browser chrome'
+}
+
+for ($i = $startNum; $i -lt $endNum; $i += 1) {
     Write-Host -ForegroundColor Green "downloading link $($linkList[$i]),$($i-$startNum+1) of $($endNum - $startNum) "
-    if ($uploadDate){
     Invoke-Expression "$tool $paramStr    '$($linkList[$i])'   "
-    }else{
-    Invoke-Expression "$tool $paramStr  '$($linkList[$i])'"
-    }
 }
 
 Write-Host -ForegroundColor Green "download complete"
