34b61d59971ddb81ae73e9d4cf0c959ba15a184e
mudssky <mudssky@gmail.com>
2023-07-13 00:53:55 +0800

    ✨ feat: add proxy function
    Former-commit-id: e9d7edcb4b2d20a89643aeee4ce1b55807b7ec4f

--- profile/functions.psm1
+++ profile/functions.psm1
@@ -123,4 +123,33 @@ function New-Shortcut {
 }
 
 
-# Export-ModuleMember -Function *
+
+$internetSettingPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+
+function Start-Proxy() {
+	param(
+	
+		[string]$URL = 'http://127.0.0.1:8080',
+		[string]$username,
+		[SecureString]$password
+	)
+	
+	Set-ItemProperty -Path $internetSettingPath -Name ProxyServer -Value $URL
+	if ($username -and $password) {
+		Set-ItemProperty -Path $internetSettingPath -Name ProxyUser  -Value $username
+		Set-ItemProperty -Path $internetSettingPath -Name ProxyPass  -Value $password
+	}
+
+	# 启用代理服务
+	Set-ItemProperty -Path $internetSettingPath -Name ProxyEnable -Value 1
+	# 重启windows代理自动检测服务，使其生效
+	# Restart-Service -Name WinHttpAutoProxySvc
+}
+
+function Close-Proxy() {
+	# 关闭代理服务
+	Set-ItemProperty -Path $internetSettingPath -Name ProxyEnable -Value 0
+}
+
+Export-ModuleMember -Function *
+
