2d9f9b68d8653c74e0d2d72cee16c5a0b10a6c12
mudssky <mudssky@gmail.com>
2023-08-31 22:07:46 +0800

    ✨ feat: qaac convert use parall
    Former-commit-id: 9d43a0f909d3cfa2b69c674fca9f098c97ebbc37

--- losslessToQaac.ps1
+++ losslessToQaac.ps1
@@ -1,59 +1,66 @@
 
 param(
-    [string]$qaacParam='--verbose --rate keep -v320 -q2 --copy-artwork',
-    [string]$targetPath='.',
+    [string]$qaacParam = '--verbose --rate keep -v320 -q2 --copy-artwork',
+    [string]$targetPath = '.',
+    [int]$ThrottleLimit = 6,
     [switch]$nodelete,
     [switch]$he
 )
 
-if($he){
-    $qaacParam='--verbose --copy-artwork --rate keep --he -v320 -q2 '
+if ($he) {
+    $qaacParam = '--verbose --copy-artwork --rate keep --he -v320 -q2 '
 }
-$losslessFiles= Get-ChildItem -Recurse -File  -LiteralPath $targetPath | Where-Object {($_.Extension  -eq '.flac') -or ($_.Extension  -eq '.wav') }
+$losslessFiles = Get-ChildItem -Recurse -File  -LiteralPath $targetPath | Where-Object { ($_.Extension -eq '.flac') -or ($_.Extension -eq '.wav') }
 $fileCounts = $losslessFiles.Length
-Write-Host -ForegroundColor Green ('Totally found '+ $fileCounts+' lossless audio files')
+Write-Host -ForegroundColor Green ('Totally found ' + $fileCounts + ' lossless audio files')
 
-$index = 0
-foreach($losslessFile in $losslessFiles){
-    $index+=1
+
+
+# 创建同步哈希表，用于在并发的进程中统计进度
+$origin = @{index = 0 }
+$sync = [System.Collections.Hashtable]::Synchronized($origin)
+
+$losslessFiles | ForEach-Object  -ThrottleLimit $ThrottleLimit -Parallel {
+    $losslessFile = $_
+    $fileCounts = $using:fileCounts
+  
+  ($using:sync).index += 1
     Write-Host "`n"
-    $progeressPercent=[int]($index/$fileCounts*100)
-    $restCounts = $fileCounts-$index
-    #Write-Host  -BackgroundColor Gray -ForegroundColor Black ('converting '+$index +' audio file ,progressing '+[int]($index/$fileCounts*100)+'% ,'+($fileCounts-$index)+' rest files' )
-    Write-Host  -BackgroundColor Gray -ForegroundColor Black ('converting {0} audio file ,progressing {1}% , {2} rest files' -f $index,$progeressPercent,$restCounts )
+    $progeressPercent = [int](($using:sync).index / $fileCounts * 100)
+    $restCounts = $fileCounts - ($using:sync).index
+    Write-Host  -BackgroundColor Gray -ForegroundColor Black ('converting {0} audio file ,progressing {1}% , {2} rest files' -f ($using:sync).index, $progeressPercent, $restCounts )
 
-    #$audiofileName = $losslessFile.BaseName
     $audiofilePath = $losslessFile.FullName
     $audiofileExt = $losslessFile.Extension
-    #$newfilename = $audiofileName+'.m4a'
-    $newfilepath=$audiofilePath.SubString(0,$audiofilePath.Length-$audiofileExt.Length)+'.m4a'
-    Write-Host -ForegroundColor Green ('audio path:'+ $audiofilePath)
-    if($audiofileExt -eq '.wav'){
-        $commandStr = ('qaac64.exe  '+$qaacParam+' "' +$audiofilePath +'" -o "' +$newfilepath+'"')
+    $newfilepath = $audiofilePath.SubString(0, $audiofilePath.Length - $audiofileExt.Length) + '.m4a'
+    Write-Host -ForegroundColor Green ('audio path:' + $audiofilePath)
+
+    # 安装了flac解码的模块以后，qaac就可以直接接受flac文件了，所以不用通过cmd转码了和wav是一样的操作
+    #  cmd /c 'ffmpeg  -i $($a) qaac64.exe   --verbose --rate keep -v320 -q2 -loglevel quiet '
+    #cmd /c ('ffmpeg -loglevel quiet -i "'+$audiofilePath +'" -f wav - | qaac64.exe '+$qaacParam+'  - -o "'+$newfilepath+'"')
+    if ($audiofileExt -in '.wav', '.flac' ) {
+        # 目前flac与wav命令相同
+        # $commandStr = ('qaac64.exe  ' + $qaacParam + ' "' + $audiofilePath + '" -o "' + $newfilepath + '"')
+        # > $null 2>$null 分别是忽略stdout 和stderr
+        $commandStr = ('qaac64.exe  {0} "{1}" -o "{2}" > $null 2>$null' -f $qaacParam, $audiofilePath , $newfilepath)
         Write-Host $commandStr
-       Invoke-Expression $commandStr
-    }elseif($audiofileExt -eq '.flac'){
-        # 安装了flac解码的模块以后，qaac就可以直接接受flac文件了，所以不用通过cmd转码了和wav是一样的操作
-       # Write-Host $audiofilePath
-      #  cmd /c 'ffmpeg  -i $($a) qaac64.exe   --verbose --rate keep -v320 -q2 -loglevel quiet '
-      #cmd /c ('ffmpeg -loglevel quiet -i "'+$audiofilePath +'" -f wav - | qaac64.exe '+$qaacParam+'  - -o "'+$newfilepath+'"')
-      $commandStr = ('qaac64.exe  '+$qaacParam+' "' +$audiofilePath +'" -o "' +$newfilepath+'"')
-      Write-Host $commandStr
-     Invoke-Expression $commandStr
-    }else{
-            Write-Host -ForegroundColor Red 'Error'
+        Invoke-Expression $commandStr  
+    }
+    else {
+        Write-Host -ForegroundColor Red 'Error. Unsupported format'
     }
 
-
-    if(Test-Path -LiteralPath  $newfilepath ){
-        if($nodelete){
+    # 清理工作，检查新文件，删除原文件
+    if (Test-Path -LiteralPath  $newfilepath ) {
+        if ($nodelete) {
             Write-Host -BackgroundColor Yellow -ForegroundColor Green 'no-delete flag is open'
-        } else{
+        }
+        else {
             Write-Host -Verbose -ForegroundColor   Cyan 'convert finshed, deleting source audio file...'
             Remove-Item -Force -LiteralPath  $audiofilePath
         }
-    } else
-    {
+    }
+    else {
         # 新文件没有创建成功，说明转换没有成功
         Write-Host -ForegroundColor Red 'convert file failed'
         
