45eb7fa5718a7fdbf4b848ee578d6717baeb0be5
mudssky <mudssky@gmail.com>
2024-01-18 15:33:13 +0800

    ✨ feat: update gitconfig scripts
    Former-commit-id: 15be97cd743c789e96fa2dc9434625eaaf5487cd

--- gitconfig_personal.ps1
+++ gitconfig_personal.ps1
@@ -1,2 +1,11 @@
+param(
+	[switch]$local
+)
+
+if ($local) {
+	git config  user.name "mudssky"
+	git config  user.email "mudssky@gmail.com"
+	exit
+}
 git config --global user.name "mudssky"
 git config --global user.email "mudssky@gmail.com"
