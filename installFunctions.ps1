85d9dd20af69c1a754d9075477f903afab448047
mudssky <mudssky@gmail.com>
2024-12-12 11:17:05 +0800

    ✨ feat: add Save-ModuleToFile

--- installFunctions.ps1
+++
@@ -1,9 +0,0 @@
-<#
-  .SYNOPSIS
-  把通用函数模块安装到当前目录
-  .DESCRIPTION
-#>
-
-Copy-Item -Path $PSScriptRoot\profile\functions.psm1 -Destination .
-
-
---
+++ psutils/modules/pwsh.psm1
@@ -0,0 +1,43 @@
+function Save-ModuleToFile {
+    <#
+    .SYNOPSIS
+        保存当前会话加载的模块的所有函数，到指定的文件。
+    .DESCRIPTION
+        A longer description of the function, its purpose, common use cases, etc.
+    .NOTES
+        Information or caveats about the function e.g. 'This function is not supported in Linux'
+    .LINK
+        Specify a URI to a help page, this will show when Get-Help -Online is used.
+    .EXAMPLE
+        Test-MyTestFunction -Verbose
+        Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    #>
+    
+    
+    [CmdletBinding()]
+    param (
+        [string]
+        $ModuleName = 'psutils',
+        [string]
+        $ModuleOutputDirectory = '.',
+        [string]
+        $ModuleOutputName = ''
+    )
+
+    if (-not $ModuleOutputName) {
+        $ModuleOutputName = $ModuleName + '.psm1'
+    }
+
+
+
+    $functions = Get-Command -Module $ModuleName -CommandType Function
+
+    $outputFunctions = New-Object System.Collections.Generic.List[string]
+    foreach ($function in $functions) {
+        $fullFunction = $function.Name + '{' + "`n" + $function.Definition + "`n" + '}'
+        $outputFunctions.Add($fullFunction)
+    }
+
+
+    Out-File -FilePath $ModuleOutputName -InputObject ($outputFunctions -Join "`n`n") -Encoding utf8
+}
