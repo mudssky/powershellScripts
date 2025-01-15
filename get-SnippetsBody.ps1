03392b5249b8e0eab592d6317af8638cf5cd2fea
mudssky <mudssky@gmail.com>
2025-01-15 15:12:18 +0800

    ✨ feat: convert double quotes

--- get-SnippetsBody.ps1
+++ get-SnippetsBody.ps1
@@ -1,3 +1,12 @@
+function Convert-DoubleQuotes {
+	param (
+		[string]$inputString
+	)
+
+	# 使用 -replace 运算符替换双引号为转义的双引号
+	return $inputString -replace '"', '\"'
+}
+
 function Get-SnippetsBody {
 	[CmdletBinding()]
 	param (
@@ -9,9 +18,11 @@ function Get-SnippetsBody {
 	}
 	
 	process {
-		$bodyList = (Get-Clipboard ).Split('\n') | ForEach-Object { '"' + $_ + '"' }
-		$bodyList
-		Set-Clipboard -Value ($bodyList -join ',') 
+
+		$bodyList = (Get-Clipboard ).Split('\n') | ForEach-Object { '"' + (Convert-DoubleQuotes -inputString $_) + '"' }
+		$res = $bodyList -join ','
+		$res
+		Set-Clipboard -Value $res
 
 	}
 	
