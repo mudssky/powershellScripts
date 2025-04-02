c5c99088ca9ebd8be0bc72fc45556dbaf46704fd
mudssky <mudssky@gmail.com>
2025-04-02 10:35:37 +0800

    feat: 添加Pester测试框架及相关配置文件

---
+++ PesterConfiguration.ps1
@@ -0,0 +1,22 @@
+$config = [PesterConfiguration]@{
+    Run          = @{
+        Path     = "./psutils"
+        # 输出测试结果对象，因为我不需要解析结果对象，所以关掉
+        # PassThru = $True
+        PassThru = $False
+    }
+    CodeCoverage = @{
+        Enabled      = $true
+        Path         = "./psutils/modules/*.psm1"
+        # OutputPath   = "./coverge.xml"
+        OutputFormat = 'CoverageGutters'
+    }
+    TestResult   = @{
+        Enabled    = $true
+        OutputPath = "./testResults.xml"
+        # OutputFormat = 'NUnitXml'
+    }
+
+}
+
+$config
---
+++ install.ps1
@@ -0,0 +1,5 @@
+# 安装测试框架
+Install-Module -Name Pester -Force
+
+# 载入配置文件
+./profile/profile.ps1 -loadProfile
