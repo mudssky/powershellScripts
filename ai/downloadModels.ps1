a47273784bd6103553c3a00852dacb9a07c8089a
mudssky <mudssky@gmail.com>
2025-05-07 20:59:54 +0800

    feat: 添加下载AI模型的PowerShell脚本
    新增一个PowerShell脚本 `downloadModels.ps1`，用于自动下载指定的AI模型。脚本中包含了一个模型列表，通过循环调用 `ollama pull` 命令来下载每个模型。该脚本简化了模型下载的流程，提高了开发效率。

---
+++ ai/downloadModels.ps1
@@ -0,0 +1,20 @@
+
+
+$modelList = @(
+    @{
+        ModelId = "bge-m3"
+        Name='bge-m3'
+    },
+    @{
+        ModelId = "qwen3:4b"
+        Name='qwen3'
+    },
+    @{
+        ModelId = "gemma3:4b"
+        Name='gemma3'
+    }
+)
+
+foreach ($model in $modelList) {
+        ollama pull $model.ModelId
+}
