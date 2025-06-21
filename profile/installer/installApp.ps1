2b4c90f9435fc767da7af06fc375f16e54d983d6
mudssky <mudssky@gmail.com>
2025-06-21 10:00:49 +0800

    feat(installer): 重构安装脚本并添加应用配置
    重构安装脚本以支持从配置文件自动安装开发工具
    添加 apps-config.json 配置文件定义各类包管理器应用
    优化包管理器初始化逻辑和错误处理

--- profile/installer/installApp.ps1
+++ profile/installer/installApp.ps1
@@ -1,25 +1,17 @@
 
 <#
 .Synopsis
-	判断环境变量中是否存在可执行程序
+    自动安装开发环境所需的各种命令行工具
 .DESCRIPTION
-   详细描述
+    通过 Chocolatey、Scoop、Cargo 等包管理器自动检测并安装开发工具
 .EXAMPLE
-   如何使用此 cmdlet 的示例
+    .\installApp.ps1
+    执行脚本安装所有配置的工具
 .EXAMPLE
-   另一个如何使用此 cmdlet 的示例
-.INPUTS
-   到此 cmdlet 的输入(如果有)
-.OUTPUTS
-   来自此 cmdlet 的输出(如果有)
+    .\installApp.ps1 -WhatIf
+    预览将要执行的安装操作
 .NOTES
-   一般注释
-.COMPONENT
-   此 cmdlet 所属的组件
-.ROLE
-   此 cmdlet 所属的角色
-.FUNCTIONALITY
-   最准确描述此 cmdlet 的功能
+    需要管理员权限安装 Chocolatey，Scoop 需要非管理员权限
 #>
 
 [CmdletBinding(SupportsShouldProcess)]
@@ -29,274 +21,95 @@ param(
 
 . $PSScriptRoot/loadModule.ps1
 
-$installListMap = @{
-	choco  = @(
-		# 'twinkle-tray' 	# 一个调节屏幕亮度的软件，win10的亮度调节可太垃圾了。
-		# 'eartrumpet' # 替代win10的音量调节
-		'bat'
-		#可以代替linux下的cat
-		'fd'
-		#搜索用的命令行工具
-		'lsd'
-		# 代替linux的ls命令
-
-		# 命令行跑分工具
-		'hyperfine'
-	);
-	scoop  = @(
-		# 用scoop安装会导致安装不上node
-		# @{
-		# 	name    = 'nvm'
-		# 	cliName = 'nvm'
-		# }
-		@{
-			name        = 'fnm'
-			cliName     = 'fnm'
-			description = 'nvm的rust替代，跨平台node版本管理'
-		},
-		@{
-			name = 'git'
-		},	
-		@{
-			name       = 'sudo'
-			cliName    = 'sudo'
-			desciption = 'win提权'
-		},	
-		@{
-			name    = 'starship'
-			cliName = 'starship'
-		},	
-		@{
-			name    = 'ffmpeg'
-			cliName = 'ffmpeg'
-		},	
-		@{
-			name = 'fzf'
-			# go编写的模糊查找神器
-		},	
-		@{
-			name    = 'ripgrep'
-			cliName = 'rg'
-			# rust正则查找神奇rg
-		},
-		@{
-
-			name = 'cht'
-			# https://github.com/chubin/cheat.sh
-		},	
-		@{
-			name    = 'aria2'
-			cliName = 'aria2c'
-		},	
-		@{
-			name = 'vhs'
-		},
-		# @{
-		# 	name = 'duf'
-		# 	# 录制终端操作到gif
-		# },
-		# @{
-		# 	name = 'dust'
-		# 	#rust ，代替linux du
-		# }
-
-		@{
-			name = 'caddy'
-			#golang的web服务器，类似nginx
-		},
-		@{
-			name = 'jq' #命令行处理json
-		}
-		@{
-			name = 'tokei' #rust git仓库代码统计
-		}
-		@{
-			name = 'lazygit' 
-		}
-		@{
-			name = 'lazydocker' 
-		}
-		@{
-			name = 'zoxide' 
-		}
-		@{
-			name = 'scc' 
-		}
-
-		# 	@{
-		# 	name    = 'lux' #下载器，但是感觉有yt-dlp就够了
-	
-		# }
-	);
-	winget = @(
-		'eartrumpet'
-	);
-	cargo  = @(
-		# 可以用于在不同的工作空间中共享已经构建好的依赖包,提升构建速度
-		'sccache'
-		# linux sed命令行的rust实现，执行速度快2倍以上
-		'sd'
-		# 查看磁盘占用情况
-		'dust' 
-		# 统计各种语言的代码行数
-		'tokei'
-		# git仓库统计信息
-		# 'onefetch'
-		# rust 版本的top，任务管理器bottom
-		'btm'
-		# rust版本tldr 太长不看帮助文档
-		'tldr'
-		# 根据用户输入生成正则表达式
-		'grex'
-		# 更智能的cd
-		'zoxide'
-		# 命令行任务管理工具，它可以管理你的长时间运行的命令，支持顺序或并行执行。简单来说，它可以管理一个命令队列。
-		'pueue'
-		# 监听到文件变动后执行命令
-		'watchexec'
-		# 监听变动执行cargo操作，和watchexec是同一个开发者
-		'cargo-watch'
-		# 文件目录管理
-		'broot'
-		# 终端查看16进制
-		'hexyl'
-		#  文件搜索
-		# 'rg'
-		# 统计代码行数 ,用go语言开发的scc代替
-		# 'cloc'
-		
-	);
-}
 
 
-
-
-function chocoInstallApps() {
-	[CmdletBinding(SupportsShouldProcess)]
-	param(
-		[string[]]
-		$installList
-	)
-	foreach ($appName in $installList) {
-		if ( -not (Test-EXEProgram $appName)) {
-			if ($PSCmdlet.ShouldProcess( '是否安装', "未检测到$appName")) {
-				choco install $appName -y
-			}
-		}
-	}
-}
-function scoopInstallApps() {
-	[CmdletBinding(SupportsShouldProcess)]
-	param(
-		[array]
-		$installList
-	)
-	foreach ($appInfo in $installList) {
-		$cliName = $appInfo.cliName  ? $appInfo.cliName : $appInfo.name 
-		$appName = $appInfo.name
-		scoop bucket add extras
-		# echo "cliName: $cliName  , appName: $appName"
-		if ( -not (Test-EXEProgram  $cliName)) {
-			if ($PSCmdlet.ShouldProcess( '是否安装', "未检测到$appName")) {
-				scoop install $appName 
-			}
-		}
- }
-
-}
-
-
-function cargoInstallApps() {
-	[CmdletBinding(SupportsShouldProcess)]
-	param(
-		[array]
-		$installList
-	)
-
-	$specialdict = @{
-		'dust'      = @{
-			# 'bin'='dust'
-			'command' = 'cargo install du-dust';
-		};
-		'pueue'     = @{
-			'command' = 'cargo install --locked pueue';
-		};
-		'tldr'      = @{
-			'command' = 'cargo install  tealdeer';
-		};
-		'watchexec' = @{
-			'command' = 'cargo install --locked watchexec-cli';
-		};
-		# 'rg'        = @{
-		# 	'command' = 'cargo install  ripgrep';
-		# };
-		'btm'       = @{
-			'command' = 'cargo install  bottom';
-		};
-	}
-
-	foreach ($appName in $installList) {
-		if ( -not (Test-EXEProgram $appName)) {
-			if ($PSCmdlet.ShouldProcess( '是否安装', "未检测到$appName")) {
-				if ($appName -in $specialdict.Keys) {
-					Invoke-Expression $specialdict[$appName].command
-				}
-				else {				
-					cargo install $appName 
-				}
-			}
-		}
-	}
-}
-function installApps() {
+function Initialize-PackageManagers() {
 	[CmdletBinding(SupportsShouldProcess)]
 	param()
-	if ( -not (Test-EXEProgram scoop)) {
-		if ($PSCmdlet.ShouldProcess('是否安装', '未检测到scoop')) {
-			Write-Host '由于scoop禁止管理员权限安装,请先在非管理员环境安装后,再继续执行
-			执行下面的语句安装
-			Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
-			'
-			return
+	
+	# 检查并安装 Scoop
+	if (-not (Test-EXEProgram scoop)) {
+		if ($PSCmdlet.ShouldProcess('Scoop', '安装包管理器')) {
+			Write-Host '由于scoop禁止管理员权限安装,请先在非管理员环境安装后,再继续执行' -ForegroundColor Yellow
+			Write-Host '执行下面的语句安装:' -ForegroundColor Yellow
+			Write-Host 'Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression' -ForegroundColor Cyan
+			return $false
 		}
-	}
-	if (-not (Test-EXEProgram g)) {
-		# 运行g的安装脚本 ，属于go的版本管理器
-	 Invoke-WebRequest https://raw.githubusercontent.com/voidint/g/master/install.ps1 -useb | Invoke-Expression
 	}
 	
-	scoopInstallApps -installList $installListMap.scoop
-
-	if ( -not (Test-EXEProgram choco)) {
-		# Write-Host -ForegroundColor Green  '未安装choco，是否安装'
-		if ($PSCmdlet.ShouldProcess('是否安装', '未检测到choco')) {
-			[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+	# 检查并安装 Chocolatey
+	if (-not (Test-EXEProgram choco)) {
+		if ($PSCmdlet.ShouldProcess('Chocolatey', '安装包管理器')) {
+			Write-Host '正在安装 Chocolatey...' -ForegroundColor Yellow
+			[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+			Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 		}
 	}
-	# if ( -not (Test-Font fira)) {
-	# 	if ($PSCmdlet.ShouldProcess('是否安装', '未检测到firacode')) {
-	# 		$win10systemRoot = (Get-ChildItem Env:SystemRoot).Value
-	# 		$win10FontsPath = $win10systemRoot + '\Fonts'
-	# 		Expand-Archive -Path './fonts/FiraCode Windows Compatible.zip' -DestinationPath $win10FontsPath
-	# 	}
-	# }
-	# echo hello 
-	chocoInstallApps -installList $installListMap.choco
-	if ( -not (Test-EXEProgram sccache)) {
-		Write-Host -ForegroundColor Yellow '请安装sccache,用于rust编译缓存,提升新安装包的编译速度,cargo install sccache,'
+	
+	# 检查并安装 Go 版本管理器 g
+	if (-not (Test-EXEProgram g)) {
+		if ($PSCmdlet.ShouldProcess('g', '安装Go版本管理器')) {
+			Write-Host '正在安装 Go 版本管理器 g...' -ForegroundColor Yellow
+			Invoke-WebRequest https://raw.githubusercontent.com/voidint/g/master/install.ps1 -useb | Invoke-Expression
+		}
 	}
-	cargoInstallApps -installList $installListMap.cargo
+	
+	# 添加 scoop extras bucket
+	if (Test-EXEProgram scoop) {
+		try {
+			scoop bucket add extras 2>$null
+		}
+		catch {
+			# 忽略已存在的错误
+		}
+	}
+	
+	return $true
+}
 
-	# if ( -not (Test-EXEProgram node)) {
-	# 	# Write-Host -ForegroundColor Green  '未安装choco，是否安装'
-	# 	if ($PSCmdlet.ShouldProcess('是否安装', '未检测到node')) {
-	# 		# 使用scoop 安装的nvm 安装node
-	# 		nvm install lts
-	# 		nvm use lts
-	# 	}
-	# }
-
+function Install-DevelopmentTools() {
+	[CmdletBinding(SupportsShouldProcess)]
+	param(
+		[string]$ConfigPath = "$PSScriptRoot/apps-config.json"
+	)
+	
+	# 初始化包管理器
+	if (-not (Initialize-PackageManagers)) {
+		return
+	}
+	
+	# 安装各类应用
+	if (Test-EXEProgram scoop) {
+		Install-PackageManagerApps -PackageManager "scoop" -ConfigPath $ConfigPath
+	}
+	
+	if (Test-EXEProgram choco) {
+		Install-PackageManagerApps -PackageManager "choco" -ConfigPath $ConfigPath
+	}
+	
+	if (Test-EXEProgram winget) {
+		Install-PackageManagerApps -PackageManager "winget" -ConfigPath $ConfigPath
+	}
+	
+	if (Test-EXEProgram cargo) {
+		Install-PackageManagerApps -PackageManager "cargo" -ConfigPath $ConfigPath
+	}
+ else {
+		Write-Host "Cargo 未安装，跳过 Rust 工具安装" -ForegroundColor Yellow
+	}
+	
+	Write-Host "所有工具安装完成！" -ForegroundColor Green
 }
 
 
-installApps 
+function installApps() {
+	[CmdletBinding(SupportsShouldProcess)]
+	param(
+		[string]$ConfigPath = "$PSScriptRoot/apps-config.json"
+	)
+	
+	Install-DevelopmentTools -ConfigPath $ConfigPath
+}
+
+
+installApps
