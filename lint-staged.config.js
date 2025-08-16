0c6c8767c24df2da24c965bf6d15f3df93468f6b
mudssky <mudssky.gmail.com>
2025-08-16 16:06:46 +0800

    ci: 在 lint-staged 配置中添加对 Lua 文件的支持
    添加对 Lua 文件的格式化支持，使用 stylua 工具进行自动格式化

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 export default {
   '*.{ps1,psm1,psd1}': 'pwsh -File ./srcipts/Format-PowerShellCode.ps1',
   '*.{js,jsx,ts,tsx,css,html,json,jsonc}': 'biome format --write',
+  '*.lua': 'stylua',
 }
