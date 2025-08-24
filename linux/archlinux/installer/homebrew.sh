6de3ee68277f07b5d02a6aaa8ae033e1b8405bb3
mudssky <mudssky@gmail.com>
2025-08-24 20:51:08 +0800

    feat(linux/archlinux): 添加 Homebrew 安装脚本
    - 新增 Homebrew 安装脚本，实现 Arch Linux 下的 Homebrew 安装自动化
    - 提供安装后的下一步操作指南，包括添加 Homebrew 到 PATH、安装依赖等

---
+++ linux/archlinux/installer/homebrew.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# ==> Next steps:
+# - Run these commands in your terminal to add Homebrew to your PATH:
+#     echo >> /home/mudssky/.bashrc
+#     echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/mudssky/.bashrc
+#     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# - Install Homebrew's dependencies if you have sudo access:
+#     sudo pacman -S base-devel
+#   For more information, see:
+#     https://docs.brew.sh/Homebrew-on-Linux
+# - We recommend that you install GCC:
+#     brew install gcc
+# - Run brew help to get started
+# - Further documentation:
+#     https://docs.brew.sh
+
