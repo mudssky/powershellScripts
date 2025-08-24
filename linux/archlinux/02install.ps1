853ef5157fa0025611efedac0a80d44bf2312baa
mudssky <mudssky@gmail.com>
2025-08-24 20:04:48 +0800

    build: 添加 Arch Linux 安装脚本
    - 新增 02install.ps1 脚本文件，用于在 Arch Linux 系统上安装所需软件包
    - 脚本主要功能：
      - 导入自定义模块 psutils
      - 从配置文件中读取待安装的软件包列表
      - 使用 pacman 包管理器安装软件包
      - （注释掉的）检查并使用 npm 安装相关包

---
+++ linux/archlinux/02install.ps1
@@ -0,0 +1,11 @@
+
+
+$modulePath = Resolve-Path -Path (Join-Path $PSScriptRoot '../../psutils')
+Import-Module -Name $modulePath
+
+$configPath = (Resolve-Path -Path (Join-Path $PSScriptRoot '../../profile/installer/apps-config.json')).Path
+Install-PackageManagerApps -PackageManager 'pacman' -ConfigPath $configPath
+
+# if (Test-ModuleInstalled -ModuleName 'npm') {
+#     Install-PackageManagerApps -PackageManager 'npm' -ConfigPath $configPath
+# }
