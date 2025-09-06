cae212986090e8ab2e30dc6668db8c38dac40188
mudssky <mudssky@gmail.com>
2025-09-07 02:04:51 +0800

    fix(linux): 修复brew安装脚本并添加环境变量配置
    将pwsh检查改为brew检查，并封装brew安装逻辑到函数中
    添加brew环境变量自动配置功能
    移除pwsh安装，改为调用单独脚本安装

--- linux/01installHomeBrew.sh
+++ linux/01installHomeBrew.sh
@@ -1,9 +1,25 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# 判断pwsh是否安装
-if ! command -v pwsh &> /dev/null
+
+install_brew() {
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    # 检查环境变量，不存在则进行配置
+    # Warning: /home/linuxbrew/.linuxbrew/bin is not in your PATH.
+    # 检查环境变量，不存在则进行配置
+    if [ -z "$(echo $PATH | grep /home/linuxbrew/.linuxbrew/bin)" ]; then
+        echo >> /home/mudssky/.bashrc
+        echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/mudssky/.bashrc
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    fi
+}
+
+
+
+# 判断brew是否安装
+if ! command -v brew &> /dev/null
 then
-    echo "pwsh not found, install it"
-    brew install powershell
+    echo "brew not found, install it"
+    install_brew
 else
-    echo "pwsh found, skip install"
+    echo "brew found, skip install"
 fi
+
+bash ./ubuntu/installer/install_pwsh.sh
