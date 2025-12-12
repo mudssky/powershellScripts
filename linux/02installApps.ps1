3be9ff119f24458df88606cfa365c34bb030c275
mudssky <mudssky@gmail.com>
2025-12-13 01:05:48 +0800

    feat(linux): 添加基于标签的Linux应用安装过滤功能

--- linux/02installApps.ps1
+++ linux/02installApps.ps1
@@ -4,7 +4,10 @@ $modulePath = Resolve-Path -Path (Join-Path $PSScriptRoot '../psutils')
 Import-Module -Name $modulePath
 
 $configPath = (Resolve-Path -Path (Join-Path $PSScriptRoot '../profile/installer/apps-config.json')).Path
-Install-PackageManagerApps -PackageManager 'homebrew' -ConfigPath $configPath
+Install-PackageManagerApps -PackageManager 'homebrew' -ConfigPath $configPath -FilterByOS $true -TargetOS 'Linux' -FilterPredicates {
+    param($appInfo)
+    $appInfo.tag -contains 'linuxserver'
+}
 
 if (Test-EXEProgram -Name 'npm') {
     Install-PackageManagerApps -PackageManager 'npm' -ConfigPath $configPath
