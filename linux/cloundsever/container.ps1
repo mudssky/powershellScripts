4d4c5309dd3468a04017dde3efec83daedfecb0a
mudssky <mudssky@gmail.com>
2025-12-15 23:52:13 +0800

    feat(云服务器): 添加容器启动脚本用于管理常用服务
    添加 container.ps1 脚本用于启动云服务器上的常用服务，包括 rustdesk 远程控制、rustfs oss 服务、qdrant 向量数据库和 postgres 数据库

---
+++ linux/cloundsever/container.ps1
@@ -0,0 +1,20 @@
+# 这个脚本记录云服务器上值得启动的服务
+
+# 远程控制
+# rusrdesk
+Write-Host "正在启动 rustdesk 服务..."
+start-container.ps1 -ServiceName rustdesk-hbbr 
+start-container.ps1 -ServiceName rustdesk-hbbs
+
+# oss
+Write-Host "正在启动 rustfs oss 服务..."
+$DefaultPassword = Read-Host -AsSecureString -Prompt "请输入默认密码"
+start-container.ps1 -ServiceName rustfs -DefaultPassword $DefaultPassword
+
+# 向量数据库
+Write-Host "正在启动qdrant向量数据库 服务..."
+start-container.ps1 -ServiceName qdrant
+
+# 数据库
+Write-Host "正在启动postgre数据库 服务..."
+start-container.ps1 -ServiceName postgres
